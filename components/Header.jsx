9c08780e3d40f6117167adbc76c8fcb390554dc9
lipho <hello@lipho.net>
2016-10-09 14:07:01 -0700

    Update to React 15.3.2 and Material-ui 0.16.0

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,15 @@
 import React, { PropTypes, Component } from 'react';
 import TodoTextInput from './TodoTextInput';
 
-import mui, {AppBar, Styles} from 'material-ui';
-import MyRawTheme from '../src/material_ui_raw_theme_file';
+import AppBar from 'material-ui/AppBar';
 
 const defaultStyle = {
   marginLeft: 20
 };
 
 class Header extends Component {
-  static get childContextTypes() {
-    return { muiTheme: React.PropTypes.object };
-  }
-
-  getChildContext(){
-    return {  muiTheme: Styles.ThemeManager.getMuiTheme(MyRawTheme)};
+  constructor(props) {
+    super(props)
   }
 
   handleSave(text) {
@@ -36,8 +31,8 @@ class Header extends Component {
   }
 }
 
-Header.propTypes = {
+/*Header.propTypes = {
   addTodo: PropTypes.func.isRequired
-};
+};*/
 
 export default Header;
